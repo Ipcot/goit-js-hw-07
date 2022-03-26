40a69730683ac85b7e42048d41324caf60b0463b
Yuriy Pokhyl <woully@gmail.com>
2022-03-26 22:03:28 +0200

    task7 part 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -12,14 +12,14 @@ function createMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         
         return `<div class="gallery__item">
-  <a class="gallery__link" href='${original}'>
+    <a class="gallery__link" href='${original}'>
     <img
-      class="gallery__image"
-      src='${preview}'
-      data-source='${original}'
-      alt='${description}'
+        class="gallery__image"
+        src='${preview}'
+        data-source='${original}'
+        alt='${description}'
     />
-  </a>
+    </a>
 </div>`
     }).join('');
 }
@@ -38,7 +38,7 @@ function openModal(source) {
     originalImage = basicLightbox.create(`
 		<img src="${source}">
 	`)
-    
+
     originalImage.show();
 
     onModalAddKeyDownListener();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        
+        return `<a class="gallery__item" href='${original}'>
+  <img class="gallery__image" src='${preview}' alt='${description}' />
+</a>`
+    }).join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captions: true, captionsData: 'alt', captionDelay: 250,});
+
 console.log(galleryItems);
