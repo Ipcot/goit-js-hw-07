0016cfea51f8fdcc54242b992f03344611bf6f34
Yuriy Pokhyl <woully@gmail.com>
2022-03-26 18:06:05 +0200

    task7 part 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click',onImgClick)
+
+function createMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        
+        return `<div class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'
+    />
+  </a>
+</div>`
+    }).join('');
+}
+
+function onImgClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    };
+    
+    openModal(event.target.dataset.source);
+}
+
+let originalImage;
+function openModal(source) {
+    originalImage = basicLightbox.create(`
+		<img src="${source}">
+	`)
+    
+    originalImage.show();
+
+    onModalAddKeyDownListener();
+}
+
+function onModalAddKeyDownListener() {
+    window.addEventListener('keydown', onEscapePress)
+}
+
+function onEscapePress(event) {
+    if (event.code === 'Escape') {
+        originalImage.close();
+        onModalEscapePressCloseWindow();
+    };
+}
+
+function onModalEscapePressCloseWindow() {
+    window.document.removeEventListener('keydown', onEscapePress)
+}
+    
+    
