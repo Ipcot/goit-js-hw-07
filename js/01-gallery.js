0ad1773b8f090426910ca151e08d79e05835ecc4
Yuriy Pokhyl <woully@gmail.com>
2022-04-26 21:45:06 +0300

    add li

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector(".gallery");
 const galleryMarkup = createMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
-galleryContainer.addEventListener('click',onImgClick)
+galleryContainer.addEventListener("click", onImgClick);
 
 function createMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        
-        return `<div class="gallery__item">
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
     <a class="gallery__link" href='${original}'>
     <img
         class="gallery__image"
@@ -20,43 +20,51 @@ function createMarkup(galleryItems) {
         alt='${description}'
     />
     </a>
-</div>`
-    }).join('');
+</div>`;
+    })
+    .join("");
 }
 
 function onImgClick(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains('gallery__image')) {
-        return;
-    };
-    
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
     openModal(event.target.dataset.source);
+    
 }
 
 let originalImage;
 function openModal(source) {
-    originalImage = basicLightbox.create(`
+  originalImage = basicLightbox.create(`
 		<img src="${source}">
-	`)
-
-    originalImage.show();
+	`);
+    
+  originalImage.show();
 
     onModalAddKeyDownListener();
+    
 }
 
 function onModalAddKeyDownListener() {
-    window.addEventListener('keydown', onEscapePress)
+  window.addEventListener("keydown", onEscapePress);
 }
 
 function onEscapePress(event) {
-    if (event.code === 'Escape') {
-        originalImage.close();
-        onModalEscapePressCloseWindow();
-    };
+    if (event.code === "Escape") {
+    
+    originalImage.close();
+    onModalEscapePressCloseWindow();
+  }
 }
 
 function onModalEscapePressCloseWindow() {
-    window.document.removeEventListener('keydown', onEscapePress)
+    window.document.removeEventListener("keydown", onEscapePress);
+    
 }
+
+function closeModal() {
+    galleryContainer.removeEventListener('click', onImgClick)
     
-    
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,9 +10,9 @@ galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 function createMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         
-        return `<a class="gallery__item" href='${original}'>
+        return `<li><a class="gallery__item" href='${original}'>
   <img class="gallery__image" src='${preview}' alt='${description}' />
-</a>`
+</a></li>`
     }).join('');
 }
 
